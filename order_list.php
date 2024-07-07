04f1165a646cb8976895dc0dcfb64289710ffa13
GeoRi01 <kentandreisabeniano01@gmail.com>
2024-07-08 05:52:21 +0800

    commit

--- order_list.php
+++ order_list.php
@@ -20,9 +20,10 @@ $orderDate = date("Y-m-d H:i:s");
 $products = $orderData['products'];
 $totalAmount = $orderData['totalAmount'];
 $tableName = $orderData['tableName'];
+$orderStatus = $orderData['orderStatus'];
 $orderItemsJson = json_encode($products);
 
-$sql = "INSERT INTO orders (table_name, order_id, order_date, total_amount, order_items) VALUES ('$tableName', '$orderID', '$orderDate', '$totalAmount', '$orderItemsJson')";
+$sql = "INSERT INTO orders (order_id, order_date, total_amount, table_name, order_status, order_items) VALUES ( '$orderID', '$orderDate', '$totalAmount', '$tableName', '$orderStatus', '$orderItemsJson')";
 if ($conn->query($sql) === TRUE) {
     echo json_encode(["status" => "success", "message" => "Order placed successfully"]);
 } else {
