57f489ac5cc4b4367161934a6714cc4710700d96
GeoRi01 <kentandreisabeniano01@gmail.com>
2024-07-08 09:41:38 +0800

    update

--- order_fetch.php
+++ order_fetch.php
@@ -6,24 +6,23 @@ $username = "root";
 $password = "";
 $dbname = "mardobs";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch data from the orders table
-$sql = "SELECT order_id, order_date, table_name, total_amount, order_items FROM orders";
+$sql = "SELECT * FROM orders";
 $result = $conn->query($sql);
 
 $orders = array();
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        
         $order_items = json_decode($row['order_items'], true);
         $row['quantity'] = count($order_items);
+        $row['order_items'] = $order_items; 
         $orders[] = $row;
     }
 }
