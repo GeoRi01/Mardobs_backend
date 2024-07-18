fb771f87b7cb7d3a9575dd4057e6e01be103e0f9
GeoRi01 <kentandreisabeniano01@gmail.com>
2024-07-18 09:27:02 +0800

    update

---
+++ order_status_fetch.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "34.143.244.112";
+$username = "dobal";
+$password = "dobal2024";
+$dbname = "mardobs";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT orders_table, orders_status FROM orders WHERE orders_status != 'Completed'";
+$result = $conn->query($sql);
+
+$orders = array();
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $orders[] = $row;
+  }
+}
+
+echo json_encode($orders);
+
+$conn->close();
+?>
